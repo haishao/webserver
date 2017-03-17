92da14c3265d0b4867451bc23d57949fd612ff31
Hai Shao <hai@halotechnologies.com>
2017-03-16 18:46:13 -0700

    json request/response

--- server.go
+++ server.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "html"
     "log"
     "net/http"
     "github.com/gorilla/mux"
+    "encoding/json"
 )
+
+type Input struct {
+    Id      string
+    Name    string
+}
+
+type Output struct {
+    Id      string
+    Message string
+}
 
 func main() {
     router := mux.NewRouter().StrictSlash(true)
@@ -15,5 +24,16 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    var input Input
+    if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+        panic(err)
+    }
+
+    output := Output{Id: input.Id, Message: "results here ..."}
+
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+    if err := json.NewEncoder(w).Encode(output); err != nil {
+        panic(err)
+    }
 }
